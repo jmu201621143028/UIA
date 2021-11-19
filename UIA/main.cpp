30c6d90468b2f6f9e9eb20f42d8e3f826c6ccf69
jmu201621143028 <46437566+jmu201621143028@users.noreply.github.com>
2021-11-19 16:27:53 +0800

    Add files via upload

--- UIA/main.cpp
+++ UIA/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	UIA uiauto;
-
+#ifdef SCROLL
 	std::vector<VecCond> cond{ {NamePropertyId, L"计算器"},
 							   {FrameworkIdPropertyId, L"XAML"},
 							   {AutomationIdPropertyId, L"NavView"},
@@ -18,7 +18,6 @@ int main()
 	varProp.vt = VT_BSTR;
 	scroll->get_CurrentLocalizedControlType(&varProp.bstrVal);
 
-
 	IUIAutomationScrollPattern * scrollpatten;
 	double Percent;
 	scroll->GetCurrentPattern(UIA_ScrollPatternId, (IUnknown**)&scrollpatten);
@@ -27,5 +26,34 @@ int main()
 	scrollpatten->Scroll(ScrollAmount_NoAmount, ScrollAmount_LargeIncrement);
 	scrollpatten->get_CurrentVerticalScrollPercent(&Percent);
 	std::cout << Percent << std::endl;
+#endif
+
+	std::vector<VecCond> cond{ {NamePropertyId, L"计算器"},
+							   {FrameworkIdPropertyId, L"XAML"},
+							   {ClassNamePropertyId, L"LandmarkTarget"},
+							   {ClassNamePropertyId, L"ListView"},
+							   {NamePropertyId, L"位移位"},
+							   {AutomationIdPropertyId, L"bitShiftButton"}
+	};
+	IUIAutomationElement * Toggle = uiauto.FinElemByCond(cond);
+	VARIANT varProp;
+	varProp.vt = VT_BSTR;
+	Toggle->get_CurrentLocalizedControlType(&varProp.bstrVal);
+	IUIAutomationTogglePattern * togglepatten;
+	Toggle->GetCurrentPattern(UIA_TogglePatternId, (IUnknown **)&togglepatten);
+	togglepatten->Toggle();
+	Sleep(800);
+	cond = {{NamePropertyId, L"计算器"},
+			{FrameworkIdPropertyId, L"XAML"},
+			{NamePropertyId, L"弹出窗口"},
+			{AutomationIdPropertyId, L"BitShiftFlyout"},
+			{AutomationIdPropertyId, L"logicalShiftButton"},
+	};
+	IUIAutomationElement * Button = uiauto.FinElemByCond(cond);
+	IUIAutomationSelectionItemPattern * sip;
+	Button->GetCurrentPattern(UIA_SelectionItemPatternId, (IUnknown **)&sip);
+	sip->Select();
+	Sleep(1000);
+	//Sleep(10000);
 	return 0;
 }
